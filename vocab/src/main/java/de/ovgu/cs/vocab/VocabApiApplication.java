e58c2e121d2ef5da13787ed2ab2c86bbd8ab7e2f
Jonas Hielscher <jonas.hielscher@hotmail.de>
2020-06-21 13:17:17 +0200

    chore: created basic Spring project.

---
+++ vocab/src/main/java/de/ovgu/cs/vocab/VocabApiApplication.java
@@ -0,0 +1,13 @@
+package de.ovgu.cs.vocab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocabApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocabApiApplication.class, args);
+	}
+
+}
